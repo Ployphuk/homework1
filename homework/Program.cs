c19b73c05be400f3fda9360f80726d9f0cda7b91
Ployphuk <111555274+Ployphuk@users.noreply.github.com>
2022-09-08 21:32:58 +0700

    Add files via upload

---
+++ homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program{
+    public static void Main(string[] args){
+        Shop info = new Shop();
+        info.InputName();
+        info.InputNumber();
+        info.InputOwner();
+        info.InputValue();
+
+        PrintInformation(info);
+}
+    
+    public static void PrintInformation(Shop shop){
+    Console.WriteLine("—--------- Shop Information —-----");
+    Console.WriteLine("Name : {0}",shop.InputName);
+    Console.WriteLine("Number : {0}",shop.InputNumber);
+    Console.WriteLine("Owner Name : {0} ",shop.InputOwner);
+    Console.WriteLine("Registered Value : {0}",shop.InputValue);
+    Console.WriteLine("—-----------------------------------------------------");
+}
+}
